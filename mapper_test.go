521c6dc6c327b03827640d0a499b513c79f477b7
Nofeatures Onlybugs <nofeaturesonlybugs@gmail.com>
2022-01-27 10:30:30 -0800

    Comment some Mapper tests.
    The commented tests are being replaced with tests or examples on
    BoundMapping or PreparedMapping as appropriate.

--- mapper_test.go
+++ mapper_test.go
@@ -1,7 +1,6 @@
 package set_test
 
 import (
-	"database/sql"
 	"fmt"
 	"reflect"
 	"strings"
@@ -184,139 +183,141 @@ func TestMapper_Map_TaggedFieldsOnly(t *testing.T) {
 	}
 }
 
-func TestMapper_Bind(t *testing.T) {
-	chk := assert.New(t)
-	//
-	{
-		type CommonDb struct {
-			Pk          int    `db:"pk" json:"id"`
-			CreatedTime string `db:"created_tmz" json:"created_time"`
-			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
-		}
-		type Person struct {
-			CommonDb
-			Name string
-			Age  int
-		}
-		var data Person
-		mapper := &set.Mapper{
-			Elevated: set.NewTypeList(CommonDb{}),
-		}
-		bound, err := mapper.Bind(&data)
-		chk.NoError(err)
-		//
-		field, err := bound.Field("Pk")
-		chk.NoError(err)
-		chk.NotNil(field)
-		err = field.To(10)
-		chk.NoError(err)
-		chk.Equal(10, data.Pk)
-		//
-		err = bound.Set("Pk", "20")
-		chk.NoError(err)
-		chk.Equal(20, data.Pk)
-		//
-		err = bound.Set("CreatedTime", "created")
-		chk.NoError(err)
-		chk.Equal("created", data.CreatedTime)
-		//
-		err = bound.Set("UpdatedTime", "updated")
-		chk.NoError(err)
-		chk.Equal("updated", data.UpdatedTime)
-		//
-		field, err = bound.Field("NotFound")
-		chk.ErrorIs(err, set.ErrUnknownField)
-		chk.Nil(field)
-	}
-	//
-	{ // Test case where CommonDb is embedded but pointer and data is pointer and we pass address of data
-		type CommonDb struct {
-			Pk          int    `db:"pk" json:"id"`
-			CreatedTime string `db:"created_tmz" json:"created_time"`
-			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
-		}
-		type Person struct {
-			*CommonDb
-			Name string
-			Age  int
-		}
-		var data *Person
-		mapper := &set.Mapper{
-			Elevated: set.NewTypeList(CommonDb{}),
-		}
-		bound, err := mapper.Bind(&data)
-		chk.NoError(err)
-		//
-		field, err := bound.Field("Pk")
-		chk.NoError(err)
-		chk.NotNil(field)
-		err = field.To(10)
-		chk.NoError(err)
-		chk.Equal(10, data.Pk)
-		//
-		err = bound.Set("Pk", "20")
-		chk.NoError(err)
-		chk.Equal(20, data.Pk)
-		//
-		err = bound.Set("CreatedTime", "created")
-		chk.NoError(err)
-		chk.Equal("created", data.CreatedTime)
-		//
-		err = bound.Set("UpdatedTime", "updated")
-		chk.NoError(err)
-		chk.Equal("updated", data.UpdatedTime)
-		//
-		field, err = bound.Field("NotFound")
-		chk.ErrorIs(err, set.ErrUnknownField)
-		chk.Nil(field)
-	}
-}
+// TODO RM
+// func TestMapper_Bind(t *testing.T) {
+// 	chk := assert.New(t)
+// 	//
+// 	{
+// 		type CommonDb struct {
+// 			Pk          int    `db:"pk" json:"id"`
+// 			CreatedTime string `db:"created_tmz" json:"created_time"`
+// 			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
+// 		}
+// 		type Person struct {
+// 			CommonDb
+// 			Name string
+// 			Age  int
+// 		}
+// 		var data Person
+// 		mapper := &set.Mapper{
+// 			Elevated: set.NewTypeList(CommonDb{}),
+// 		}
+// 		bound, err := mapper.Bind(&data)
+// 		chk.NoError(err)
+// 		//
+// 		field, err := bound.Field("Pk")
+// 		chk.NoError(err)
+// 		chk.NotNil(field)
+// 		err = field.To(10)
+// 		chk.NoError(err)
+// 		chk.Equal(10, data.Pk)
+// 		//
+// 		err = bound.Set("Pk", "20")
+// 		chk.NoError(err)
+// 		chk.Equal(20, data.Pk)
+// 		//
+// 		err = bound.Set("CreatedTime", "created")
+// 		chk.NoError(err)
+// 		chk.Equal("created", data.CreatedTime)
+// 		//
+// 		err = bound.Set("UpdatedTime", "updated")
+// 		chk.NoError(err)
+// 		chk.Equal("updated", data.UpdatedTime)
+// 		//
+// 		field, err = bound.Field("NotFound")
+// 		chk.ErrorIs(err, set.ErrUnknownField)
+// 		chk.Nil(field)
+// 	}
+// 	//
+// 	{ // Test case where CommonDb is embedded but pointer and data is pointer and we pass address of data
+// 		type CommonDb struct {
+// 			Pk          int    `db:"pk" json:"id"`
+// 			CreatedTime string `db:"created_tmz" json:"created_time"`
+// 			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
+// 		}
+// 		type Person struct {
+// 			*CommonDb
+// 			Name string
+// 			Age  int
+// 		}
+// 		var data *Person
+// 		mapper := &set.Mapper{
+// 			Elevated: set.NewTypeList(CommonDb{}),
+// 		}
+// 		bound, err := mapper.Bind(&data)
+// 		chk.NoError(err)
+// 		//
+// 		field, err := bound.Field("Pk")
+// 		chk.NoError(err)
+// 		chk.NotNil(field)
+// 		err = field.To(10)
+// 		chk.NoError(err)
+// 		chk.Equal(10, data.Pk)
+// 		//
+// 		err = bound.Set("Pk", "20")
+// 		chk.NoError(err)
+// 		chk.Equal(20, data.Pk)
+// 		//
+// 		err = bound.Set("CreatedTime", "created")
+// 		chk.NoError(err)
+// 		chk.Equal("created", data.CreatedTime)
+// 		//
+// 		err = bound.Set("UpdatedTime", "updated")
+// 		chk.NoError(err)
+// 		chk.Equal("updated", data.UpdatedTime)
+// 		//
+// 		field, err = bound.Field("NotFound")
+// 		chk.ErrorIs(err, set.ErrUnknownField)
+// 		chk.Nil(field)
+// 	}
+// }
 
-func TestMapper_Bind_Rebind(t *testing.T) {
-	chk := assert.New(t)
-	//
-	{
-		type CommonDb struct {
-			Pk          int    `db:"pk" json:"id"`
-			CreatedTime string `db:"created_tmz" json:"created_time"`
-			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
-		}
-		type Person struct {
-			CommonDb
-			Name string
-			Age  int
-		}
-		var a, b Person
-		var other CommonDb
-		mapper := &set.Mapper{
-			Elevated: set.NewTypeList(CommonDb{}),
-		}
-		bound, err := mapper.Bind(&a)
-		chk.NoError(err)
-		//
-		err = bound.Set("Pk", 10)
-		chk.NoError(err)
-		chk.Equal(10, a.Pk)
-		//
-		bound.Rebind(&b)
-		err = bound.Set("Pk", 20)
-		chk.NoError(err)
-		chk.Equal(20, b.Pk)
-		//
-		chk.NotEqual(a.Pk, b.Pk)
-		//
-		var didPanic bool
-		func() {
-			defer func() {
-				if r := recover(); r != nil {
-					didPanic = true
-				}
-			}()
-			bound.Rebind(&other)
-		}()
-		chk.Equal(true, didPanic)
-	}
-}
+// TODO RM
+// func TestMapper_Bind_Rebind(t *testing.T) {
+// 	chk := assert.New(t)
+// 	//
+// 	{
+// 		type CommonDb struct {
+// 			Pk          int    `db:"pk" json:"id"`
+// 			CreatedTime string `db:"created_tmz" json:"created_time"`
+// 			UpdatedTime string `db:"modified_tmz" json:"modified_time"`
+// 		}
+// 		type Person struct {
+// 			CommonDb
+// 			Name string
+// 			Age  int
+// 		}
+// 		var a, b Person
+// 		var other CommonDb
+// 		mapper := &set.Mapper{
+// 			Elevated: set.NewTypeList(CommonDb{}),
+// 		}
+// 		bound, err := mapper.Bind(&a)
+// 		chk.NoError(err)
+// 		//
+// 		err = bound.Set("Pk", 10)
+// 		chk.NoError(err)
+// 		chk.Equal(10, a.Pk)
+// 		//
+// 		bound.Rebind(&b)
+// 		err = bound.Set("Pk", 20)
+// 		chk.NoError(err)
+// 		chk.Equal(20, b.Pk)
+// 		//
+// 		chk.NotEqual(a.Pk, b.Pk)
+// 		//
+// 		var didPanic bool
+// 		func() {
+// 			defer func() {
+// 				if r := recover(); r != nil {
+// 					didPanic = true
+// 				}
+// 			}()
+// 			bound.Rebind(&other)
+// 		}()
+// 		chk.Equal(true, didPanic)
+// 	}
+// }
 
 func TestMapperBindCollision(t *testing.T) {
 	chk := assert.New(t)
@@ -440,190 +441,92 @@ func TestMapperCodeCoverage(t *testing.T) {
 	}
 }
 
-func ExampleMapper() {
-	type CommonDb struct {
-		Pk          int    `t:"pk"`
-		CreatedTime string `t:"created_time"`
-		UpdatedTime string `t:"updated_time"`
-	}
-	type Person struct {
-		CommonDb `t:"common"`
-		Name     string `t:"name"`
-		Age      int    `t:"age"`
-	}
-	var data Person
-	{
-		mapper := &set.Mapper{
-			Elevated: set.NewTypeList(CommonDb{}),
-			Join:     "_",
-		}
-		mapping := mapper.Map(&data)
-		fmt.Println(strings.Replace(mapping.String(), "\t\t", " ", -1))
-	}
-	{
-		fmt.Println("")
-		fmt.Println("lowercase with dot separators")
-		mapper := &set.Mapper{
-			Join:      ".",
-			Transform: strings.ToLower,
-		}
-		mapping := mapper.Map(&data)
-		fmt.Println(strings.Replace(mapping.String(), "\t\t", " ", -1))
-	}
-	{
-		fmt.Println("")
-		fmt.Println("specify tags")
-		mapper := &set.Mapper{
-			Join: "_",
-			Tags: []string{"t"},
-		}
-		mapping := mapper.Map(&data)
-		fmt.Println(strings.Replace(mapping.String(), "\t\t", " ", -1))
-	}
+// TODO RM
+// func ExampleMapper_Bind() {
+// 	type CommonDb struct {
+// 		Pk          int
+// 		CreatedTime string
+// 		UpdatedTime string
+// 	}
+// 	type Person struct {
+// 		CommonDb
+// 		Name string
+// 		Age  int
+// 	}
+// 	Print := func(p Person) {
+// 		fmt.Printf("Person: pk=%v created=%v updated=%v name=%v age=%v\n", p.Pk, p.CreatedTime, p.UpdatedTime, p.Name, p.Age)
+// 	}
+// 	data := []Person{{}, {}}
+// 	Print(data[0])
+// 	Print(data[1])
+// 	//
+// 	mapper := &set.Mapper{
+// 		Elevated: set.NewTypeList(CommonDb{}),
+// 	}
+// 	bound, err := mapper.Bind(&data[0])
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	bound.Set("Pk", 10)
+// 	bound.Set("CreatedTime", "-5h")
+// 	bound.Set("UpdatedTime", "-2h")
+// 	bound.Set("Name", "Bob")
+// 	bound.Set("Age", 30)
+// 	if err := bound.Err(); err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	//
+// 	bound.Rebind(&data[1])
+// 	bound.Set("Pk", 20)
+// 	bound.Set("CreatedTime", "-15h")
+// 	bound.Set("UpdatedTime", "-12h")
+// 	bound.Set("Name", "Sally")
+// 	bound.Set("Age", 20)
+// 	if err := bound.Err(); err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	//
+// 	Print(data[0])
+// 	Print(data[1])
 
-	// Output: [0 0] Pk
-	// [0 1] CreatedTime
-	// [0 2] UpdatedTime
-	// [1] Name
-	// [2] Age
-	//
-	// lowercase with dot separators
-	// [0 0] commondb.pk
-	// [0 1] commondb.createdtime
-	// [0 2] commondb.updatedtime
-	// [1] name
-	// [2] age
-	//
-	// specify tags
-	// [0 0] common_pk
-	// [0 1] common_created_time
-	// [0 2] common_updated_time
-	// [1] name
-	// [2] age
-}
+// 	// Output: Person: pk=0 created= updated= name= age=0
+// 	// Person: pk=0 created= updated= name= age=0
+// 	// Person: pk=10 created=-5h updated=-2h name=Bob age=30
+// 	// Person: pk=20 created=-15h updated=-12h name=Sally age=20
 
-func ExampleMapper_treatAsScalar() {
-	type T struct {
-		S string
-		T time.Time
-		N sql.NullString
-	}
+// }
 
-	mapping := set.DefaultMapper.Map(T{})
-	if mapping.Get("T") != nil {
-		fmt.Println("T is mapped because time.Time is automatically treated as a scalar.")
-	}
-	if mapping.Get("N") == nil {
-		fmt.Println("N can not be found because sql.NullString was not treated as a scalar.")
-	}
-	if mapping.Get("N_Valid") != nil {
-		fmt.Println("N_Valid was mapped because the exported fields in sql.NullString were mapped.")
-	}
+// func ExampleBoundMapping() {
+// 	type Person struct {
+// 		First string
+// 		Last  string
+// 	}
+// 	values := []map[string]string{
+// 		{"first": "Bob", "last": "Smith"},
+// 		{"first": "Sally", "last": "Smith"},
+// 	}
+// 	mapper := &set.Mapper{
+// 		Transform: strings.ToLower,
+// 	}
+// 	var people []Person
+// 	bound, err := mapper.Bind(&Person{})
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	for _, m := range values {
+// 		person := Person{}
+// 		bound.Rebind(&person)
+// 		for fieldName, fieldValue := range m {
+// 			bound.Set(fieldName, fieldValue)
+// 		}
+// 		if err := bound.Err(); err != nil {
+// 			fmt.Println(err.Error())
+// 		}
+// 		people = append(people, person)
+// 	}
+// 	fmt.Println(people[0].First, people[0].Last)
+// 	fmt.Println(people[1].First, people[1].Last)
 
-	//
-	// Now we'll treat sql.NullString as a scalar when mapping.
-	mapper := &set.Mapper{
-		TreatAsScalar: set.NewTypeList(sql.NullString{}),
-	}
-	mapping = mapper.Map(T{})
-	if mapping.Get("N") != nil {
-		fmt.Println("N is now mapped to the entire sql.NullString member.")
-		v, _ := set.V(&T{}).FieldByIndex(mapping.Get("N"))
-		fmt.Printf("N's type is %v\n", v.Type())
-	}
-
-	// Output: T is mapped because time.Time is automatically treated as a scalar.
-	// N can not be found because sql.NullString was not treated as a scalar.
-	// N_Valid was mapped because the exported fields in sql.NullString were mapped.
-	// N is now mapped to the entire sql.NullString member.
-	// N's type is sql.NullString
-}
-
-func ExampleMapper_Bind() {
-	type CommonDb struct {
-		Pk          int
-		CreatedTime string
-		UpdatedTime string
-	}
-	type Person struct {
-		CommonDb
-		Name string
-		Age  int
-	}
-	Print := func(p Person) {
-		fmt.Printf("Person: pk=%v created=%v updated=%v name=%v age=%v\n", p.Pk, p.CreatedTime, p.UpdatedTime, p.Name, p.Age)
-	}
-	data := []Person{{}, {}}
-	Print(data[0])
-	Print(data[1])
-	//
-	mapper := &set.Mapper{
-		Elevated: set.NewTypeList(CommonDb{}),
-	}
-	bound, err := mapper.Bind(&data[0])
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	bound.Set("Pk", 10)
-	bound.Set("CreatedTime", "-5h")
-	bound.Set("UpdatedTime", "-2h")
-	bound.Set("Name", "Bob")
-	bound.Set("Age", 30)
-	if err := bound.Err(); err != nil {
-		fmt.Println(err.Error())
-	}
-	//
-	bound.Rebind(&data[1])
-	bound.Set("Pk", 20)
-	bound.Set("CreatedTime", "-15h")
-	bound.Set("UpdatedTime", "-12h")
-	bound.Set("Name", "Sally")
-	bound.Set("Age", 20)
-	if err := bound.Err(); err != nil {
-		fmt.Println(err.Error())
-	}
-	//
-	Print(data[0])
-	Print(data[1])
-
-	// Output: Person: pk=0 created= updated= name= age=0
-	// Person: pk=0 created= updated= name= age=0
-	// Person: pk=10 created=-5h updated=-2h name=Bob age=30
-	// Person: pk=20 created=-15h updated=-12h name=Sally age=20
-
-}
-
-func ExampleBoundMapping() {
-	type Person struct {
-		First string
-		Last  string
-	}
-	values := []map[string]string{
-		{"first": "Bob", "last": "Smith"},
-		{"first": "Sally", "last": "Smith"},
-	}
-	mapper := &set.Mapper{
-		Transform: strings.ToLower,
-	}
-	var people []Person
-	bound, err := mapper.Bind(&Person{})
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	for _, m := range values {
-		person := Person{}
-		bound.Rebind(&person)
-		for fieldName, fieldValue := range m {
-			bound.Set(fieldName, fieldValue)
-		}
-		if err := bound.Err(); err != nil {
-			fmt.Println(err.Error())
-		}
-		people = append(people, person)
-	}
-	fmt.Println(people[0].First, people[0].Last)
-	fmt.Println(people[1].First, people[1].Last)
-
-	// Output: Bob Smith
-	// Sally Smith
-}
+// 	// Output: Bob Smith
+// 	// Sally Smith
+// }
